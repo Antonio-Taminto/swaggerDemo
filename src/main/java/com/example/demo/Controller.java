a934a6a765797789b943dc8153a0b7715a66c37d
Antonio Taminto <taminto.a@gmail.com>
2024-02-22 17:35:21 +0100

    Initial commit

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1")
 public class Controller {
-    @GetMapping
-    public String nome(@RequestParam String stringa){
-        return stringa;
+        @GetMapping("/richiedinome")
+    public String richiediNome(@RequestParam String nome){
+        return "il nome è : " + nome;
     }
+    @PostMapping("/richiediuser")
+    public User richiediUser(@RequestBody User user){
+        return user;
+    }
+
 }
